330687d982fe3f1afbc19773f7b9cf0f9073d500
hugotonette <hugo.tonette@gmail.com>
2024-08-28 18:40:32 -0300

    part 1.6-1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,33 +2,81 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad;
+
+  if (all != 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticsLine text="Good" value={props.good} />
+          <StatisticsLine text="Neutral" value={props.neutral} />
+          <StatisticsLine text="Bad" value={props.bad} />
+          <StatisticsLine text="All" value={all} />
+          <StatisticsLine
+            text="Average"
+            value={((props.good - props.bad) / all).toFixed(1)}
+          />
+          <StatisticsLine
+            text="Positive"
+            value={((props.good * 100) / all).toFixed(1)}
+          />
+        </tbody>
+      </table>
+    );
+  } else {
+    return (
+      <div>
+        <p>No feedback yet...</p>
+      </div>
+    );
+  }
+};
+
+const StatisticsLine = (props) => {
+  return (
+    <tr>
+      <td> {props.text} </td>
+      <td> {props.value} </td>
+    </tr>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1> Give feedback </h1>
+        <div>
+          <Button handleClick={handleGoodClick} text="good" />
+          <Button handleClick={handleNeutralClick} text="neutral" />
+          <Button handleClick={handleBadClick} text="bad" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>Statistics</h1>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
-}
+};
 
 export default App;
